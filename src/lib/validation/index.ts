c1b866fd754f5dc6831b0290b2eb90d6c78e0932
psnogueira <psnogueira.e@gmail.com>
2023-12-18 21:29:57 -0300

    Correção no index.ts

--- src/lib/validation/index.ts
+++ src/lib/validation/index.ts
@@ -2,7 +2,7 @@ import * as z from "zod"
 
 export const SignupValidation = z.object({
     name: z.string().min(2, { message: 'Nome muito curto.' }),
-    username: z.string().min(2, { message:'Usuario muito curto.' }).max(24, { message: 'Usuario grande demais.' }),
+    username: z.string().min(2, { message:'Usuário muito curto.' }).max(24, { message: 'Usuário grande demais.' }),
     email: z.string().email({ message: 'Email Inválido.' }),
     password: z.string().min(8, { message:'Senha precisa ter pelo menos 8 caracteres.' })
   })
